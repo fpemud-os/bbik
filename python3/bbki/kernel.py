49ae89051c480c76cc7ac2bfdc010d649ced3036
fpemud <fpemud@sina.com>
2021-08-15 18:40:46 +0800

    fix

--- python3/bbki/kernel.py
+++ python3/bbki/kernel.py
@@ -24,6 +24,7 @@ import os
 import re
 import kmod
 import pylkcutil
+import pkg_resources
 from .util import Util
 from .util import TempChdir
 from .boot_entry import BootEntry
@@ -126,7 +127,13 @@ class KernelInstaller:
             rulesDict["head"] = buf
 
         # build-in rules
-        rulesDict.update(self._kernelCfgRules())
+        for fn in sorted(pkg_resources.resource_listdir(__name__, "kernel-config-rules")):
+            if not fn.endswith(".rules"):
+                continue
+            m = re.fullmatch(r'([0-9]+-)?(.*)\.rules', fn)
+            if m is not None:
+                rname = m.group(2)
+            rulesDict[rname] = pkg_resources.resource_string(__name__, os.path.join("kernel-config-rules", fn)).decode("iso8859-1")
 
         # addon rules
         for addon_item in self._addonAtomList:
@@ -136,7 +143,15 @@ class KernelInstaller:
         # sysadmin rules
         rulesDict["custom"] = ""            # FIXME
 
-        self._generateKernelCfgRulesFile(kcfgRulesTmpFile, rulesDict)
+        # generate .config file
+        with open(kcfgRulesTmpFile, "w") as f:
+            for name, buf in rulesDict.items():
+                f.write("## %s ######################\n" % (name))
+                f.write("\n")
+                f.write(buf)
+                f.write("\n")
+                f.write("\n")
+                f.write("\n")
 
         # debug feature
         if True:
@@ -160,29 +175,6 @@ class KernelInstaller:
         self._executorDict[self._kernelAtom].exec_kernel_install()
         for item in self._addonAtomList:
             self._executorDict[item].exec_kernel_addon_install()
-
-    def _kernelCfgRules(self):
-        ret = dict()
-        for fullfn in sorted(glob.glob(os.path.join(FmConst.dataDir, "kernel-config-rules", "*.rules"))):
-            fn = os.path.basename(fullfn)
-            rname = fn[:len(".rules") * -1]
-            m = re.fullmatch("[0-9]+-(.*)", rname)
-            if m is not None:
-                rname = m.group(1)
-            with open(fullfn, "r") as f:
-                ret[rname] = f.read()
-        return ret
-
-    def _generateKernelCfgRulesFile(self, filename, *kargs):
-        with open(filename, "w") as f:
-            for kcfgRulesMap in kargs:
-                for name, buf in kcfgRulesMap.items():
-                    f.write("## %s ######################\n" % (name))
-                    f.write("\n")
-                    f.write(buf)
-                    f.write("\n")
-                    f.write("\n")
-                    f.write("\n")
 
 
 class BootEntryWrapper:
