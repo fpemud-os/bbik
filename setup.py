1a6277d9e38c2cc4ffcbe4c3ded5dbdaf8ffd5da
fpemud <fpemud@sina.com>
2021-08-15 19:03:19 +0800

    fix

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         'bbki': 'python3/bbki',
     },
     package_data={
-        'bbki': ['kernel-config-rules/*', 'initramfs/init', 'initramfs/lvm-lv-activate'],
+        'bbki': ['kernel-config-rules/*', 'initramfs/init', 'initramfs/lvm-lv-activate', 'initramfs/*.c'],
     },
     cmdclass={
         'install': custom_install,
